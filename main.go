eca53056f982b026af5e63f17d8f6829bcb08ddf
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-12-17 22:29:28 +0800

    feat: 🎸 added notify close app

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 
 	"github.com/mrspec7er/go-http-std/app"
 )
@@ -9,7 +11,10 @@ import (
 func main() {
 	app := app.New()
 
-	err := app.Start(context.TODO())
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	err := app.Start(ctx)
 	if err != nil {
 		panic(err)
 	} 
