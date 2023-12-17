fb94338c79ea6222f4b43ee05d8bac1de31afaf2
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-12-17 15:56:23 +0800

    feat: 🎸 Added Chi router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/mrspec7er/go-chi
 
 go 1.21.5
+
+require github.com/go-chi/chi/v5 v5.0.10 // indirect
--- main.go
+++ main.go
@@ -3,20 +3,27 @@ package main
 import (
 	"log"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
+	router := chi.NewRouter()
+	router.Use(middleware.Logger)
 	server := &http.Server{
 		Addr: ":8080",
-		Handler: http.HandlerFunc(handler),
+		Handler: router,
 	}
 
 	log.Println("Server Start On Port :", 8080)
+
+	router.Get("/", handler)
+
 	err := server.ListenAndServe()
 	if err != nil {
 		panic(err.Error())
 	}
-
 }
 
 func handler(w http.ResponseWriter, r *http.Request)  {
