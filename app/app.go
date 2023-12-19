ab4c1bca8ec7cde7beac9b3f581813f9566a9946
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-12-19 23:03:32 +0800

    refactor: 💡 move db connection to utils

--- app/app.go
+++ app/app.go
@@ -2,13 +2,10 @@ package app
 
 import (
 	"context"
-	"fmt"
 	"net/http"
-	"os"
 
 	"github.com/mrspec7er/go-http-std/app/model"
-	"gorm.io/driver/postgres"
-	"gorm.io/gorm"
+	"github.com/mrspec7er/go-http-std/app/utils"
 )
 
 type App struct {
@@ -21,20 +18,12 @@ func New() *App {
 	}
 }
 
-func DBConnection() *gorm.DB {
-	credentials := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable TimeZone=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_TIMEZONE"))
-
-	connection, err := gorm.Open(postgres.Open(credentials), &gorm.Config{})
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return connection
-}
 
 func (a *App) Start(ctx context.Context) error {
-	DB := DBConnection()
-	DB.AutoMigrate(&model.Movie{})
+
+	utils.DBConnection()
+	
+	utils.DB.AutoMigrate(&model.Movie{})
 	
 	server := &http.Server{
 		Addr: ":8080",
---
+++ app/utils/db.go
@@ -0,0 +1,23 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DBConnection() {
+	credentials := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=disable TimeZone=%s", os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_TIMEZONE"))
+
+	connection, err := gorm.Open(postgres.Open(credentials), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+
+	DB = connection
+
+}
