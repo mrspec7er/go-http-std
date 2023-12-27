9526c5fb3f6b950d7f799e0991df830fef840fca
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-12-27 23:18:50 +0800

    feat: 🎸 added cast router

---
+++ app/cast/route.go
@@ -0,0 +1,15 @@
+package cast
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+func Routes(router chi.Router)  {
+	controller := &CastController{}
+
+	router.Post("/", controller.HandlerCreate)
+	router.Get("/", controller.HandlerGetAll)	
+	router.Get("/{id}", controller.HandlerGetOne)
+	router.Put("/{id}", HandlerUpdate)
+	router.Delete("/{id}", HandlerDelete)
+}
