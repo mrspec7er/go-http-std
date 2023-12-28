32213e28cac326fbe8090bae9d5e936bb68de3a4
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-12-28 20:36:13 +0800

    feat: 🎸 added user controller

---
+++ app/user/controller.go
@@ -0,0 +1,70 @@
+package user
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/mrspec7er/go-http-std/app/repository"
+	"github.com/mrspec7er/go-http-std/app/utils"
+)
+
+
+type DirectorController struct {
+	Director DirectorService
+}
+
+
+func(c *DirectorController) HandlerCreate(w http.ResponseWriter, r *http.Request)  {
+	var Director repository.Director
+    if err := json.NewDecoder(r.Body).Decode(&Director); err != nil {
+        utils.BadRequestHandler(w)
+        return
+    }
+
+	status, err := c.Director.Create(Director)
+	if err != nil {
+		utils.InternalServerErrorHandler(w, status, err)
+		return
+	}
+
+	utils.MutationSuccessResponse(w, "Successfully insert new Director")
+
+}
+
+func (c *DirectorController) HandlerGetAll(w http.ResponseWriter, r *http.Request) {
+	result, status, err := c.Director.GetAll()
+	if err != nil {
+		utils.InternalServerErrorHandler(w, status, err)
+		return
+	}
+
+	utils.GetSuccessResponse(w, nil, result, nil)
+}
+
+func (c *DirectorController) HandlerGetOne(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id");
+	formattedId, err := strconv.ParseUint(id, 10, 32)
+	if err != nil {
+		utils.BadRequestHandler(w)
+        return
+	}
+
+	result, status, err := c.Director.GetOne(uint(formattedId))
+	if err != nil {
+		utils.InternalServerErrorHandler(w, status, err)
+		return
+	}
+
+	utils.GetSuccessResponse(w, nil, result, nil)
+}
+
+func HandlerUpdate(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Update a Movie")
+}
+
+func HandlerDelete(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Delete a Movie")
+}
