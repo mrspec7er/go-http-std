9dd6f4a4496cc5ca2e45480c46e8e266ca4c835a
Kusuma Sandi <wijayakusumasandi@gmail.com>
2023-12-28 20:36:38 +0800

    feat: 🎸 added user route

---
+++ app/user/route.go
@@ -0,0 +1,15 @@
+package user
+
+import (
+	"github.com/go-chi/chi/v5"
+)
+
+func Routes(router chi.Router)  {
+	controller := &UserController{}
+
+	router.Post("/", controller.HandlerCreate)
+	router.Get("/", controller.HandlerGetAll)	
+	router.Get("/{id}", controller.HandlerGetOne)
+	router.Put("/{id}", HandlerUpdate)
+	router.Delete("/{id}", HandlerDelete)
+}
